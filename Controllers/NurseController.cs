12d6587ae1bb0445c823eaf643a073ad40dab429
s221406603 <s221406603@mandela.ac.za>
2024-06-15 20:32:05 +0200

    updated nurse

--- Controllers/NurseController.cs
+++ Controllers/NurseController.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WIRKDEVELOPER.Areas.Identity.Data;
+using WIRKDEVELOPER.Models;
 
 namespace WIRKDEVELOPER.Controllers
 {
     public class NurseController : Controller
     {
+        private readonly ApplicationDBContext Context;
+        public NurseController(ApplicationDBContext DbContext)
+        {
+            Context = DbContext;
+        }
         public IActionResult Index()
         {
             return View();
+        }
+        public IActionResult ViewSchedule()
+        {
+            IEnumerable<Patient> patient = Context.patients;
+            return View(patient);
+        }
+        public IActionResult ViewAdmission()
+        {
+            IEnumerable<Patient> patient = Context.patients;
+            return View(patient);
         }
     }
 }
