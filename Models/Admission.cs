d5d094d1ebbc03d54ad0010c9fa76dc7dca6f5a3
s221406603 <s221406603@mandela.ac.za>
2024-08-02 11:06:56 +0200

    Nurse

--- Models/Admission.cs
+++ Models/Admission.cs
@@ -1,16 +1,37 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WIRKDEVELOPER.Models
 {
     public class Admission
     {
+        [Key]
         public int AdmissionID { get; set; }
-        public int PatientID { get; set; }
+        [DisplayName("Patient Name")]
+        public int PatientName { get; set; }
+        [ForeignKey("PatientID")] 
+        [DisplayName("Gender")]
+        public int PatientGender { get; set; }
         [ForeignKey("PatientID")]
-        public string Address1 { get; set; } 
+        [DisplayName("Email")]
+        public int PatientEmail { get; set; }
+        [ForeignKey("PatientID")] 
+        [DisplayName("Province")]
+        public int PatientProvince { get; set; }
+        [ForeignKey("PatientID")]
+        [DisplayName("Address 1")]
+        public string Address1 { get; set; }
+        [ForeignKey("PatientID")]
+        [DisplayName("Address 2")]
         public string Address2 { get; set; }
+        [ForeignKey("PatientID")] 
+        [DisplayName("City")]
         public string City { get; set; }
-        public string Suburb { get; set; } 
+        [ForeignKey("PatientID")]
+        [DisplayName("Surbub")]
+        public string Surbub { get; set; }
+        [ForeignKey("PatientID")]
         public int BedID { get; set; }
         [ForeignKey("BedID")]
         public int ConditionID { get; set; }
