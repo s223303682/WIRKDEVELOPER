70a79362225576acd121cae11ce83ef6814fc71e
s221406603 <s221406603@mandela.ac.za>
2024-08-08 00:02:58 +0200

    Nurse

--- Models/VitalRanges.cs
+++ Models/VitalRanges.cs
@@ -14,10 +14,16 @@ namespace WIRKDEVELOPER.Models
 		[Required]
 		[DisplayName("Visit Date")]
 		public DateTime? Date { get; set; } = DateTime.Now;
+		[Required]
+		[DisplayName("Visit Time")]
+		public DateTime? Time { get; set; } = DateTime.Now;
 
 		[Required]
 		[DisplayName("Vital Type")]
 		public string? Vital { get; set; }
+		[Required]
+		[DisplayName("Patient Name")]
+		public string? PatientName { get; set; }
 
 		[Required]
 		[DisplayName("Minimum Range")]
@@ -26,6 +32,12 @@ namespace WIRKDEVELOPER.Models
 		[Required]
 		[DisplayName("Maximum Range ")]
 		public string? Maximumrange { get; set; }
+		[Required]
+		[DisplayName("Weight")]
+		public string? Weight { get; set; }
+		[Required]
+		[DisplayName("Height")]
+		public string? Height { get; set; }
 
 		[Required]
 		public string? Units { get; set; }
