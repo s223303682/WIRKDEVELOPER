7f453a2e7b3f9ee9e73f519c11392ee81cb70813
s221406603 <s221406603@mandela.ac.za>
2024-08-15 19:16:46 +0200

    Nurse

--- Models/MedicationAdministration.cs
+++ Models/MedicationAdministration.cs
@@ -5,13 +5,16 @@ namespace WIRKDEVELOPER.Models
     public class MedicationAdministration
     {
         public int MedicationAdministrationID { get; set; }
-        public int PatientID { get; set; }
+        public int PatientName { get; set; }
         [ForeignKey("PatientID")]
         public DateTime IssuedDate { get; set; }
-        public DateTime TimeIssued { get; set; } 
+        //public DateTime TimeIssued { get; set; } 
         public int Quantity { get; set; } 
         public string Status { get; set; }
-        public int MedicationID { get; set; }
+        public string Instructions { get; set; }
+        public string PrescriberName { get; set; }
+        public string PrescriberPhone { get; set; }
+        public int MedicationName { get; set; }
         [ForeignKey("MedicationID")]
        
         public int StaffId { get; set; }
