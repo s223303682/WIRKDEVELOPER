a44c9e8dbd72709136eae95640be8d4893f65e34
s221406603@mandela.ac.za <DELL@DESKTOP-M4IPOUT>
2024-09-18 08:48:03 +0200

    Nurse

--- Models/Admission.cs
+++ Models/Admission.cs
@@ -11,51 +11,52 @@ namespace WIRKDEVELOPER.Models
         public string PatientName { get; set; }
         [ForeignKey("PatientID")]
         [DisplayName("Patient Name")]
-        public string PatientSurname { get; set; }
-        [ForeignKey("PatientID")]
-        [DisplayName("Patient Surname")]
-        public string PatientGender { get; set; }
-        [ForeignKey("PatientID")]
-        [DisplayName("Gender")] 
-        public string MedicationName { get; set; }
-        [ForeignKey("CurrentMedicationID")]
-        [DisplayName("Current Medication")] 
-        public int TreatmentCode { get; set; }
-        [ForeignKey("TreatmentID")]
-        [DisplayName("Treatment Code")] 
-        public int PatientPhone { get; set; }
-        [ForeignKey("PatientID")]
-        [DisplayName("Phone")]
-        public string PatientEmail { get; set; }
-        [ForeignKey("PatientID")] 
-        [DisplayName("Email")]
-        public string PatientProvince { get; set; }
-        [ForeignKey("PatientID")]
-        [DisplayName("Province")]
-        public string Address1 { get; set; }
-        [ForeignKey("PatientID")]
-        [DisplayName("Address 1")]
-        public string Address2 { get; set; }
-        [ForeignKey("PatientID")] 
-        [DisplayName("Address 2")]
-        public string City { get; set; }
-        [ForeignKey("PatientID")]
-        [DisplayName("City")]
-        public string Surbub { get; set; }
-        [ForeignKey("PatientID")]
+        public virtual Patient Patient { get; set; }
+        //public string PatientSurname { get; set; }
+        //[ForeignKey("PatientID")]
+        //[DisplayName("Patient Surname")]
+        //public string PatientGender { get; set; }
+        //[ForeignKey("PatientID")]
+        //[DisplayName("Gender")] 
+        //public string MedicationName { get; set; }
+        //[ForeignKey("CurrentMedicationID")]
+        //[DisplayName("Current Medication")] 
+        //public int TreatmentCode { get; set; }
+        //[ForeignKey("TreatmentID")]
+        //[DisplayName("Treatment Code")] 
+        //public int PatientPhone { get; set; }
+        //[ForeignKey("PatientID")]
+        //[DisplayName("Phone")]
+        //public string PatientEmail { get; set; }
+        //[ForeignKey("PatientID")] 
+        //[DisplayName("Email")]
+        //public string PatientProvince { get; set; }
+        //[ForeignKey("PatientID")]
+        //[DisplayName("Province")]
+        //public string Address1 { get; set; }
+        //[ForeignKey("PatientID")]
+        //[DisplayName("Address 1")]
+        //public string Address2 { get; set; }
+        //[ForeignKey("PatientID")] 
+        //[DisplayName("Address 2")]
+        //public string City { get; set; }
+        //[ForeignKey("PatientID")]
+        //[DisplayName("City")]
+        //public string Surbub { get; set; }
+        //[ForeignKey("PatientID")]
         public int BedID { get; set; }
         [ForeignKey("BedID")]
-        public int ConditionName { get; set; }
+        public string ConditionName { get; set; }
         [ForeignKey("ConditionID")]
         public string WardName { get; set; }
         [ForeignKey("WardID")] 
-        public int AllergiesID { get; set; }
+        public string AllergiesName { get; set; }
         [ForeignKey("AlergiesID")] 
-        public int MedicationID { get; set; }
+        public string MedicationName { get; set; }
         [ForeignKey("MedicationID")] 
-        public int TreatmentID { get; set; }
+        public int TreatmentCode { get; set; }
         [ForeignKey("TreatmentID")]
-        public string PostalCode { get; set; }
+        //public string PostalCode { get; set; }
         [Required]
         [DisplayName(" Date")]
         public DateTime? Date { get; set; } = DateTime.Now;
